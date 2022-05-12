bb0ddf36fd074f373f48b4bd3f3a162be3accb95
cgawande <vikug10@gmail.com>
2022-05-13 01:20:29 +0530

    second program

---
+++ May22/2nd.js
@@ -0,0 +1,10 @@
+//javaScript simple Program to Add,multiplication,division and substractio Two Numbers 
+const num1 = 25;
+const num2 = 5;
+// addition
+sum = num1 + num2;
+multi = num1 * num2;
+div = num1 / num2;
+sub = num1 - num2;
+
+console.log("addition is " + sum + ", subtraction is " + sub + ", quotient is " + div + ", Multiplication is " + multi);
