b0045155012b1c5129b4f7d483b5faeedc8de298
cgawande <vikug10@gmail.com>
2022-06-12 09:04:50 +0530

    array

--- May22/aaray1.js
+++ May22/aaray1.js
@@ -3,3 +3,12 @@ let array = [1, 2, 3];
 console.log(array[0])
 array[0] = 3;
 console.log(array[0])
+
+// Setup
+const myArray = [
+    ["John", 23],
+    ["dog", 3]
+];
+myArray.shift();
+myArray.unshift(["Paul", 35]);
+console.log(myArray)
