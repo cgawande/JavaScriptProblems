d34545b6b811a354d7e9471f7419eb4af04b44e0
cgawande <vikug10@gmail.com>
2022-06-13 19:14:54 +0530

    array

--- May22/aaray1.js
+++ May22/aaray1.js
@@ -12,3 +12,30 @@ const myArray = [
 myArray.shift();
 myArray.unshift(["Paul", 35]);
 console.log(myArray)
+
+const months = ['Jan', 'March', 'April', 'June'];
+months.splice(2, 2)
+console.log(months)
+months.splice(1, 0, "feb")
+console.log(months)
+
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant', "deer"];
+
+console.log(animals.slice(-1));
+// expected output: Array ["deer"]
+
+console.log(animals.slice(2, 4));
+// expected output: Array ["camel", "duck"]
+
+console.log(animals.slice(1, 5));
+// expected output: Array ["bison", "camel", "duck", "elephant"]
+
+console.log(animals.slice(-2));
+// expected output: Array ["duck", "elephant"]
+
+console.log(animals.slice(2, -1));
+// expected output: Array ["camel", "duck"]
+
+console.log(animals.slice());
+// expected output: Array ["ant", "bison", "camel", "duck", "elephant"]
+console.log(animals.slice(2, -2));
