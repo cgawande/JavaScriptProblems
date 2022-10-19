fdd6db9b45543b223c471187865e82f68c433db0
cgawande <vikug10@gmail.com>
2022-10-19 11:02:26 +0530

    two sum problem

--- May22/randomnumber.js
+++ May22/randomnumber.js
@@ -1,3 +1,3 @@
 // generating a random number between 1 and 10
-const a = parseInt(Math.random() * (10 - 1) + 1)
+const a = parseInt(Math.random() * 9 + 1)
 console.log(`Random value between 1 and 10 is ${a}`);
