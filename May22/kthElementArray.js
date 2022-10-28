6983a828b22479f4bd40a9854247bc8cd5033abd
cgawande <vikug10@gmail.com>
2022-10-28 23:35:31 +0530

    problem

--- May22/kthElementArray.js
+++ May22/kthElementArray.js
@@ -11,3 +11,16 @@ function kthSmallest(arr,k){
   
 }
 console.log(kthSmallest(arr,k))
+
+//Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
+
+for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  console.log("Sorted array=>", arr);
